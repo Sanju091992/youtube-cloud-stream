1264986abffed4ec74af5bb2c21ee2e97b7d58a1
Sanju091992 <worldbestsection@gmail.com>
2025-06-13 19:47:55 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Local video को loop करके stream करें (no download)
+while true
+do
+  ffmpeg -re -stream_loop -1 -i video.mp4 -c:v libx264 -preset veryfast -b:v 3000k -c:a aac -b:a 160k -ar 44100 -f flv "rtmp://a.rtmp.youtube.com/live2/YOUR-STREAM-KEY"
+done
